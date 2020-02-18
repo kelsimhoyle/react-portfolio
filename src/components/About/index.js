40f9756d137125fed7f8200f99acc29f8757cddb
Kelsi H <kelsimhoyle@gmail.com>
2020-02-18 16:30:29 -0700

    updated to use styled-components, added client website

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,46 +1,32 @@
 import React from "react";
-import { MdTerrain, MdWhatshot, MdTrendingUp } from "react-icons/md";
-import "./style.scss";
+import { Heading, LightBackground, Blurb, ClearFix, FloatLeft, FloatRight } from "../../styled"
 
 const About = () => {
     return (
         <div id="about">
-            <div className="content">
-                <h2>About Me</h2>
-                <ul className="about-me">
-                    <li className="describe">
-                        <MdWhatshot className="list-icon" />
-                        <h5>Passionate</h5>
-                        <p>I invest myself in projects and building my knowledge.</p>
-                    </li>
-                    <li className="describe">
-                        <MdTerrain className="list-icon" />
-                        <h5>Adventurous</h5>
-                        <p>I am in my element when I am facing a challenging problem.</p>
-                    </li>
-                    <li className="describe">
-                        <MdTrendingUp className="list-icon" />
-                        <h5>Always Growing</h5>
-                        <p>I actively seek opportunities to broaden my knowledge and skills.</p>
-                    </li>
-                </ul>
-                <div className="content">
-                    <div className="clearfix">
-                        <div className="float-right right">
-                            <img src="images/portrait.jpg" alt="portrait" className="portrait" />
-                        </div>
-                        <div className="float-left left about">
-                            <p>I am a full stack web developer with a background in education. I started coding as a
-                                hobby, and it quickly turned into a passion. I jumped into the field of web devolopment
-                                because I love challenges and learning, and there isn't a better place for me to be!</p>
+            <Heading>about me.</Heading>
+            <LightBackground>
+                <ClearFix>
+                    <FloatRight>
+                        <img src="images/portrait.jpg" alt="portrait" style={{ width: "100%", height: "auto" }} />
+                    </FloatRight>
+                    <FloatLeft>
+                        <Blurb>
+                            <h4>Educator turned developer.</h4>
+                            <p>I am a full stack web developer with a background in education. I jumped into the field of web devolopment
+                                because I love challenges and learning, and there isn't a better place for me to be! I spend most of my days working on my side projects. I also enjoy volunteering with Code for Denver and working on civic projects.</p>
+                            <p>I am in my element when I am being challenged. I also love to lean a little into my education background and teach my new skills to my friends!</p>
+                        </Blurb>
+                        <Blurb>
                             <h4>Technologies:</h4>
-                            <p>JavaScript, React, React Hooks, Node.js, HTML5, CSS3, BootStrap, Materialize, JQuery, MySQL, MongoDB, NoSQL, Express,
-                                Sequelize, GraphQL, Apollo, Handlebars Templating, and always adding more to my skills.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
+                            <p>JavaScript, TypeScript, Node, React, React Hooks,  HTML5, CSS3, BootStrap, Materialize, JQuery, Express, MySQL, MongoDB, NoSQL,
+                                Sequelize, GraphQL, Apollo, Handlebars Templating, and always adding more to my skills. I am currently learning GoLang!</p>
+                        </Blurb>
+                    </FloatLeft>
+                </ClearFix>
+            </LightBackground>
+
+        </div>
     )
 }
 
---
+++ src/components/Title/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Title = ({title}) => {
+    return (
+<h2>{title}</h2>    
+)
+}
+
+export default Title
